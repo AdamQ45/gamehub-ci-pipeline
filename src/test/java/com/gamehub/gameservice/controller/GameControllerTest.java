f9242ece49aa1a0dd490c5363c9118c96bd24fc1
Adam <enlldmx@E-CND21914RH.localdomain>
2026-04-19 14:27:08 +0100

    Fix MockBean import for Spring Boot 3.2.x

--- src/test/java/com/gamehub/gameservice/controller/GameControllerTest.java
+++ src/test/java/com/gamehub/gameservice/controller/GameControllerTest.java
@@ -8,7 +8,7 @@ import com.gamehub.gameservice.repository.PurchaseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.bean.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
